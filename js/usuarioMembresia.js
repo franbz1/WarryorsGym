220ca282510f9cac8192a7c9598088c0848bc683
franbz1 <90373114+franbz1@users.noreply.github.com>
2022-07-11 08:47:15 -0500

    post cambio de el botton submit

---
+++ js/usuarioMembresia.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+   // on ready
+});
+
+async function registrarUsuario() {
+    alert('funciona');
+      let datos = {};
+      datos.nombre = document.getElementById('txtNombreMebresia').value;
+      datos.email = document.getElementById('txtEmailMebresia').value;
+      datos.telefono = document.getElementById('txtTelefonoMebresia').value;
+      datos.mensaje = document.getElementById('txtMensajeMembresia').value;
+
+    const request = await fetch('api/usuariomembresia', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(datos)
+      });
+      alert("La cuenta fue creada con exito!");
+      window.location.href = 'login.html'
+
+}
