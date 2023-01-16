bd5704f20059501250192a9da167a342843560fa
Fran <franciscorualesp15@gmail.com>
2023-01-15 19:12:06 -0500

    terminado el prototipo

--- app.js
+++ app.js
@@ -1,42 +1,107 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-
 //app middelwares
-
 
 express.urlencoded({ extended: true });
 app.use(bodyParser.json());
-app.use('/public', express.static('public'));
+app.use("/public", express.static("public"));
 
 //app routes
-app.get('/warryorsgym1',(req,res)=>{
+app.get("/warryorsgym1", (req, res) => {
+  res.sendFile(__dirname + `/public/index.html`);
+});
 
-    res.sendFile(__dirname+`/public/index.html`);
+app.post("/warryors1post1", (req, res) => {
+  const data = req.body;
 
-})
+  if (Object.keys(data).length > 0) {
+    res.status(200).send("Datos recibidos!");
+    console.log("Nombre: ", data.name);
+    console.log("Email: ", data.email);
+    console.log("Mensaje: ", data.message);
+    console.log(req.body);
 
-app.post('/warryors1post1',(req,res)=>{
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+          user: "warryorsgym@gmail.com",
+          pass: "dsxciwkvtegohmej",
+      },
+  })
 
-    const data = req.body;
+  let mailOptions = {
+    from: "warryorsgym@gmail.com",
+    to: "franciscorualesp15@gmail.com",
+    subject: "Nuevo formulario de contacto",
+    html: `
+    <p>Nombre: ${data.name}</p>
+    <p>Email: ${data.email}</p>
+    <p>Mensaje: ${data.message}</p>
+    `,
+  };
 
-    if (Object.keys(data).length > 0) {
-        res.send('Formulario enviado');
-        console.log("Nombre: ", data.name);
-        console.log("Email: ", data.email);
-        console.log("Mensaje: ", data.message);
-        console.log(req.body);
-
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
     }
-    else{
-        console.log("objeto vacio");
+  });
+
+  } else {
+    console.log("objeto vacio");
+  }
+});
+
+app.post("/warryors1post2", (req, res) => {
+  const data = req.body;
+
+  if (Object.keys(data).length > 0) {
+    res.status(200).send("Datos recibidos!");
+    console.log("Nombre: ", data.name);
+    console.log("Email: ", data.email);
+    console.log("Mensaje: ", data.message);
+    console.log("Telefono: ", data.phone);
+    console.log(req.body);
+
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+          user: "warryorsgym@gmail.com",
+          pass: "dsxciwkvtegohmej",
+      },
+  })
+
+  let mailOptions = {
+    from: "warryorsgym@gmail.com",
+    to: "franciscorualesp15@gmail.com",
+    subject: "Nuevo formulario de contacto",
+    html: `
+    <p>Nombre: ${data.name}</p>
+    <p>Email: ${data.email}</p>
+    <p>Telefono: ${data.phone}</p>
+    <p>Mensaje: ${data.message}</p>
+    `,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
     }
+  });
 
-})
+  } else {
+    console.log("objeto vacio");
+  }
+});
 
-app.listen(port,()=>{
-    console.log(`app start at port:${port}`);
-})
+app.listen(port, () => {
+  console.log(`app start at port:${port}`);
+});
--- public/js/asyncForm.js
+++ public/js/asyncForm.js
@@ -1,9 +1,9 @@
-const form = document.getElementById("form1");
+const form1 = document.getElementById("form1");
 
-form.addEventListener("submit", async (event) => {
+form1.addEventListener("submit", async (event) => {
   event.preventDefault();
 
-  const formData = new FormData(form);
+  const formData = new FormData(form1);
   const data = {};
   for (const [key, value] of formData.entries()) {
       data[key] = value;
@@ -12,6 +12,40 @@ form.addEventListener("submit", async (event) => {
 
   try {
     const response = await fetch("/warryors1post1", {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+  });
+    if (response.ok) {
+      alert("Datos enviados con éxito!");
+    } else {
+      alert("Ocurrió un problema con el envío de datos!");
+  }
+} catch (error) {
+  console.error(error);
+}
+});
+
+
+//___________________________________________________________________
+
+
+const form2 = document.getElementById("form2");
+
+form2.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(form2);
+  const data = {};
+  for (const [key, value] of formData.entries()) {
+      data[key] = value;
+  }
+  delete data["submit-button"];
+
+  try {
+    const response = await fetch("/warryors1post2", {
       method: "POST",
       headers: {
           "Content-Type": "application/json"
